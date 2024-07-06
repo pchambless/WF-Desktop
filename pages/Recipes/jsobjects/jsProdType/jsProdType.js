2016c71acb9e6bdc2000da3484a9d59a49979e0c
Paul Chambless <pc7900@gmail.com>
2024-07-06 20:31:20 +0000

    Edit mode changes and recipe List

--- pages/Recipes/jsobjects/jsProdType/jsProdType.js
+++ pages/Recipes/jsobjects/jsProdType/jsProdType.js
@@ -1,6 +1,8 @@
 export default {
 async select () {
 	await storeValue('prodTypeID',sel_Type.selectedOptionValue)
+	await storeValue('editMode','')
 	await prod_dd.run()
+	await entity_List.clear()
 }
 }
