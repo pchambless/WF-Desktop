3b2c9209e9c407db9454939b3b2a828b00573978
Paul Chambless <pc7900@gmail.com>
2024-07-10 19:07:05 +0000

    Tightening and standardizing interface and processes

--- pages/Ingredients/jsobjects/jsSelect/jsSelect.js
+++ pages/Ingredients/jsobjects/jsSelect/jsSelect.js
@@ -2,9 +2,11 @@ export default {
 	async selectType () {
 		await storeValue('ingrTypeID', sel_Type.selectedOptionValue)
 		entity_List.run()	
+		ingr_List.run()
 	},
 	async selectEntity () {
 		await storeValue('editMode','Edit')
 		await storeValue('ingrID', tbl_Entity.selectedRow.id)
+
 	}
 }
--- pages/Product Batch/jsobjects/jsSelect/jsSelect.js
+++ pages/Product Batch/jsobjects/jsSelect/jsSelect.js
@@ -9,12 +9,13 @@ export default {
 	async selectProd () {
 		await storeValue("editMode","")
 		await storeValue('prodID',sel_prod.selectedOptionValue)
+		await prod_Select.run()
 		await entity_List.run()
 		await batch_Last.run()
 		await batchIngr_Map.clear()
 		await batchTask_List.clear()
 		await rcpe_List.run()
-		prod_Select.run()
+
 	},
 	async selectEntity () {
 		await tbl_Entity.selectedRowIndex === -1 ? jsForm.add : jsForm.edit()
