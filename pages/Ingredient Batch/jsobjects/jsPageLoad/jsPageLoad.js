7d1b5ec96e427795e59f038ddaca23ef9b76220e
Paul Chambless <pc7900@gmail.com>
2024-07-11 12:10:07 +0000

    Add Hint to Dashboard.

--- pages/Ingredient Batch/jsobjects/jsPageLoad/jsPageLoad.js
+++ pages/Ingredient Batch/jsobjects/jsPageLoad/jsPageLoad.js
@@ -2,9 +2,9 @@ export default {
 	async load () {
 		await storeValue('active','Y')
 		await ingrType_dd.run()
-		await sel_Type.setSelectedOption(appsmith.store.ingrTypeID)
+		await sel_Type.setSelectedOption(appsmith.store.ingrTypeID || '') 
 		await ingr_dd.run()
-		await sel_ingr.setSelectedOption(appsmith.store.ingrID)
+		await sel_ingr.setSelectedOption(appsmith.store.ingrID || '') 
 		await entity_List.run()
 		await entity_Last.run()
 	}
