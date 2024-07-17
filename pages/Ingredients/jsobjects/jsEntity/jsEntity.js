9f0f641b820a55ef99f577b0cb6d0e3d78a00c74
Paul Chambless <pc7900@gmail.com>
2024-07-17 14:30:31 +0000

    Tweaks to Ingredients

--- pages/Ingredients/jsobjects/jsEntity/jsEntity.js
+++ pages/Ingredients/jsobjects/jsEntity/jsEntity.js
@@ -2,6 +2,7 @@ export default {
 	async editModeRun  () {
 		tbl_Entity.selectedRowIndex === -1 || !entity_List.data.length ? entity_Add.run() : entity_Edit.run()
 		await entity_List.run()
+		await ingr_List.run()
 	},
 	async editMode  () {
 		tbl_Entity.selectedRowIndex === -1 || !entity_List.data.length ? jsForm.add() : jsForm.edit()
