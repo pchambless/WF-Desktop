83b0257e0f960d790571c8300d01e40bb9853042
Paul Chambless <pc7900@gmail.com>
2026-03-28 12:07:38 +0000

    created product_batch_worksheet.

--- pages/Product Batch/jsobjects/n8nSelect/n8nSelect.js
+++ pages/Product Batch/jsobjects/n8nSelect/n8nSelect.js
@@ -4,5 +4,14 @@ export default {
 	await n8nvals.setVals();
 	// create recipe report
   await grid_product_batches.run();
+  },
+product_batch_id: async () => {
+	await storeValue("setValsBody", [{ param_name: "product_batch_id", param_val: tbl_Entity.selectedRow.id }]);
+	await n8nvals.setVals();
+	// create recipe report
+  await grid_product_batches.run();
+  await rpt_batch_wrksht_hdr.run();
+  await rpt_batch_wrksht_ingr.run();
+	await rpt_batch_wrksht_tasks.run();
   }
 }
