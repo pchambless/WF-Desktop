3448ff479344f995df7f7afc09916ab3fa7f7dff
Paul Chambless <pc7900@gmail.com>
2026-03-31 19:36:42 +0000

    Login page reworked.  Added password

--- pages/Ingredient Batch/jsobjects/n8nSelect/n8nSelect.js
+++ pages/Ingredient Batch/jsobjects/n8nSelect/n8nSelect.js
@@ -1,18 +1,22 @@
 export default {
-	ingredient_type_id: async () => {
-	await storeValue("setValsBody", [{ param_name: "ingredient_type_id", param_val: selIngredientType.selectedOptionValue }]);
-	await n8nVals.setVals();
-  dd_ingredient.run()
-  },
+  page_load: async () => {
+	await storeValue("setClearsBody", ["ingredient_batch_id","ingredient_id"]);
+  await n8nVals.clearVals();
+  await grid_ingredient_batches.run();
+	await rpt_srce_trgt_hdr.run();
+  await rpt_srce_trgt_body.run();
+	},
 	ingredient_id: async () => {
-	await storeValue("setValsBody", [{ param_name: "ingredient_id", param_val: selIngredient.selectedOptionValue }]);
-	await n8nVals.setVals();
+  await storeValue("setValsBody", [{ param_name: "ingredient_id", param_val: selIngredient.selectedOptionValue }]);
+  await n8nVals.setVals();
   grid_ingredient_batches.run()
-  },
+  await storeValue("setClearsBody", ["ingredient_batch_id"]);
+  await n8nVals.clearVals();
+},
 	ingredient_batch_id: async () => {
-	await storeValue("setValsBody", [{ param_name: "ingredient_id", param_val: tbl_Entity.selectedRow.id }]);
+	await storeValue("setValsBody", [{ param_name: "ingredient_batch_id", param_val: tbl_Entity.selectedRow.id }]);
 	await n8nVals.setVals();
   await rpt_srce_trgt_hdr.run();
-  await rpt_srce_trgt_body.run()
+  await rpt_srce_trgt_body.run();
   },
 }
